e82b025ab74b5c0416c5c91a6375d9b24551e909
paulgligor <paul.gligor@gmail.com>
2018-12-06 15:35:41 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HubConnection, HubConnectionBuilder } from '@aspnet/signalr';
+import { Message } from './models/message.model';
+import * as signalR from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'example-project-signalr-client';
+export class AppComponent implements OnInit {
+  public messages: Message[] = [];
+  public message: Message = new Message();
+  private connection: HubConnection;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const builder = new signalR.HubConnectionBuilder();
+    this.connection = builder
+      .withUrl('http://localhost:52525/hubs/messaging')
+      .build();
+
+    this.connection.start()
+    .then(() => {
+      console.log('started');
+      //
+    });
+
+    this.connection.on('BroadcastMessage', (message: Message) => {
+      console.log('BroadcastMessage', message);
+      this.messages.push(message);
+    });
+
+    this.messages.push({ name: 'init', text: 'hello, it\'s me init'  });
+  }
+
+  sendMessage() {
+    this.connection.invoke('BroadcastMessage', this.message);
+    this.message = new Message();
+  }
 }
